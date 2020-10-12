a21b128dae9ff64774598f678757eab500677161
lajtosdavid11 <lajtos.david11@gmail.com>
2020-10-12 12:48:46 +0200

    Add project files.

---
+++ hajo/Hajo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hajo
+{
+    class Hajo
+    {
+        public int Regiszter { get; private set; }
+        public string Nev { get; private set; }
+        public string Tipus { get; private set; }
+
+        public int Utas { get; private  set; }
+        public int Dij { get; private set; }
+
+        public Hajo(string sor)
+
+        {
+            string[] a = sor.Split(';');
+            Regiszter = Convert.ToInt32(a[0]);
+            Nev = a[1];
+            Tipus = a[2];
+            Utas = Convert.ToInt32(a[3]);
+            Dij = Convert.ToInt32(a[4]);
+
+            //7;Durbincs;WIN-22;5;24000
+        }
+    }
+}
---
+++ hajo/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+/* 
+ * 1. Hány hajó van?
+ * 2.Melyik hajó(k) a legdrágábbak
+ * 3.Névsorban a hajok.txt-be kell kiiratni a hajók nevét
+ * 4. melyik hajótipusból van a legtöbb
+ */
+namespace hajo
+{
+    class Program
+    {
+        static List<Hajo> hajok = new List<Hajo>();
+        static List<string> sorba = new List<string>();
+
+        static void Beolvasas()
+        {
+            StreamReader sr = new StreamReader("hajo.csv");
+            string b = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                hajok.Add(new Hajo(sr.ReadLine()));
+            }
+            sr.Close();
+
+        }
+
+
+        static void elso()
+        {
+            int ossze = 0;
+            while (ossze < hajok.Count)
+            {
+                ossze++;
+            }
+            Console.WriteLine("Hajók száma {0}",ossze);
+        }
+
+        static void kettes()
+        {
+            int max = 0;
+            foreach (var m in hajok)
+            {
+                if (m.Dij >= max)
+                {
+                    max = m.Dij;
+                }
+            }
+            foreach (var n in hajok)
+            {
+                if (n.Dij == max)
+                {
+                    Console.WriteLine(n.Nev);
+                }
+            }
+           
+        }
+        static void harmas()
+        {
+            StreamWriter sw = new StreamWriter("hajok.txt");
+
+            for (int i = 0; i < hajok.Count; i++)
+            {
+                sorba.Add(hajok[i].Nev);
+            }
+            sorba.Sort();
+
+            for (int i = 0; i < sorba.Count; i++)
+            {
+                sw.WriteLine(sorba[i]);
+            }
+
+
+
+            sw.Close();
+        }
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            //foreach (var b in hajok)
+            //{
+            //    Console.WriteLine(b.Regiszter +" "+ b.Nev + " "+ b.Tipus +" " + b.Utas + " "+ b.Dij);
+            //}
+            elso();
+            kettes();
+            harmas();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
